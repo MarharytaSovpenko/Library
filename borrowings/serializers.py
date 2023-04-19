4eb4abd65538a82360450878ebb9468b8ca405f6
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-04-20 01:10:30 +0200

    Create borrowings serializers and views

---
+++ borrowings/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from books.serializers import BookSerializer
+from borrowings.models import Borrowing
+from users.serializers import UserSerializer
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = "__all__"
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = serializers.CharField(source="book.title", read_only=True)
+    user = serializers.CharField(source="user.title", read_only=True)
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookSerializer(many=False, read_only=True)
+    user = UserSerializer(many=False, read_only=True)
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from typing import Type
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingListSerializer
+
+
+class BorrowingViewSet(ModelViewSet):
+    queryset = Borrowing.objects.select_related("book", "user")
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self) -> Type[BorrowingSerializer]:
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
