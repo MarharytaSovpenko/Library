fc2d3f7d7d3cea6166d80b4d2063492f8b21b2d9
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-04-25 00:31:30 +0200

    Correct flake8 errors

--- borrowings/views.py
+++ borrowings/views.py
@@ -3,7 +3,11 @@ from typing import Type
 from rest_framework.viewsets import ModelViewSet
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer, BorrowingDetailSerializer, BorrowingListSerializer
+from borrowings.serializers import (
+    BorrowingSerializer,
+    BorrowingDetailSerializer,
+    BorrowingListSerializer
+)
 
 
 class BorrowingViewSet(ModelViewSet):
@@ -16,9 +20,13 @@ class BorrowingViewSet(ModelViewSet):
             user_id = self.request.query_params.get("user_id")
             if user_id is not None:
                 queryset = queryset.filter(user_id=user_id)
-            return queryset.filter(is_active=self.request.query_params.get("is_active", True))
-        return self.queryset.filter(user=self.request.user,
-                                    is_active=self.request.query_params.get("is_active", True))
+            return queryset.filter(
+                is_active=self.request.query_params.get("is_active", True)
+            )
+        return self.queryset.filter(
+            user=self.request.user,
+            is_active=self.request.query_params.get("is_active", True)
+        )
 
     def get_serializer_class(self) -> Type[BorrowingSerializer]:
         if self.action == "list":
