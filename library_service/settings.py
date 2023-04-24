08811be0f7879754f04f7f80884e1b2b9b3c023c
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-04-25 01:15:49 +0200

    Rewrite the queryset in the book view

--- library_service/settings.py
+++ library_service/settings.py
@@ -120,7 +120,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "cinema.permissions.IsAuthenticatedOrReadOnly",
+        "rest_framework.permissions.IsAuthenticated",
     ]
 }
 
